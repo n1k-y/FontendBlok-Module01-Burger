856a57c40a2a228f591d48f16551ec4fa42a2372
n1k-y <nik_yura92@mail.ru>
2025-02-15 00:29:06 +0300

    DZ21

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 // Smooth Scroll
-      
+
 const links = document.querySelectorAll(".banner-btn");
 
 for (const link of links) {
-link.addEventListener("click", clickHandler);
+    link.addEventListener("click", clickHandler);
 }
 
 function clickHandler(e) {
-e.preventDefault();
-const href = this.getAttribute("href");
+    e.preventDefault();
+    const href = this.getAttribute("href");
 
-document.querySelector(href).scrollIntoView({
-    behavior: "smooth"
-});
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
 }
 
 // Button Up
 
-window.onscroll = function(){scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 const upbuttons = document.querySelectorAll(".button-up");
 
@@ -26,16 +26,16 @@ for (const upbutton of upbuttons) {
 }
 
 function clickHandler(e) {
-e.preventDefault();
-const href = this.getAttribute("href");
+    e.preventDefault();
+    const href = this.getAttribute("href");
 
-document.querySelector(href).scrollIntoView({
-    behavior: "smooth"
-});
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
 }
 
-function scrollFunction(){
-    if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
         document.getElementById('btnUp').className = 'button-up visible';
     } else {
         document.getElementById('btnUp').className = 'button-up hidden';
